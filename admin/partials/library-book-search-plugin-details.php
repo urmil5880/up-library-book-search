584dfa2ee9304c9da0a485477b335128df539d07
Urmil Patel <37611573+urmil5880@users.noreply.github.com>
2018-12-24 12:28:24 +0530

    Create library-book-search-plugin-details.php

---
+++ admin/partials/library-book-search-plugin-details.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin detail text
+ *
+ * @sing 1.0.0
+ * @link       https://profiles.wordpress.org/urmilwp
+ * @since      1.0.0
+ * @package    Library_Book_Search_Plugin
+ * @subpackage Library_Book_Search_Plugin/admin/partials
+ */
+
+?>
+<div class="wrap">
+	<h1 class="wp-heading-inline">
+		<?php esc_attr_e( 'WP Library Book Search', 'library-book-search-plugin' ); ?>
+	</h1>
+	<p>
+		<?php esc_attr_e( 'A Book Search functionality and Custom Post type with short code, books in the different parameter like Book Name, Author, Publisher and price etc. Also work with Gutenberg shortcode block.', 'library-book-search-plugin' ); ?>
+	</p>
+	<h2><?php esc_attr_e( 'ShortCode', 'library-book-search-plugin' ); ?></h2>
+	<p>
+		<?php esc_attr_e( '[book-search]', 'library-book-search-plugin' ); ?>
+	</p>
+	<p>
+		<?php esc_attr_e( 'Using this shortcode you can show library book search facility on page/post.', 'library-book-search-plugin' ); ?>
+	</p>
+</div>
