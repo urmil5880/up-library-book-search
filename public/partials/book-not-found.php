661946dd5e877030c8ebec62a5b6423c0afd1d47
Urmil Patel <37611573+urmil5880@users.noreply.github.com>
2018-12-24 12:35:16 +0530

    Create book-not-found.php

---
+++ public/partials/book-not-found.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Book Not Found HTML
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://profiles.wordpress.org/urmilwp
+ * @since      1.0.0
+ *
+ * @package    Library_Book_Search_Plugin
+ * @subpackage Library_Book_Search_Plugin/public/partials
+ */
+
+?>
+
+<tr>
+	<td colspan="6">
+		<?php esc_html_e( 'No Books Found!', 'library-book-search-plugin' ); ?>
+	</td>
+</tr>
